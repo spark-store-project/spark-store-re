ca72d3f038421ce8627bc1322942deb733325053
robin <dequanzh@usc.edu>
2021-08-03 18:54:55 -0700

    first impression made

--- ui/src/layouts/MainLayout.vue
+++ ui/src/layouts/MainLayout.vue
@@ -1,47 +1,60 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header class="native-window-draggable" elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-          style="-webkit-app-region: none;"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
+  <q-layout view="lHh Lpr lff">
+    <q-header class="native-window-draggable bg-white" elevated>
+        <q-toolbar style="height: 80px;">
+          <div class="q-pa-md" style="max-width: 350px;">
+            <q-input style="-webkit-app-region: no-drag;"
+              v-model="search"
+              debounce="500"
+              filled
+              placeholder="Search"
+            >
+              <template v-slot:prepend>
+                <q-icon name="search" />
+              </template>
+            </q-input>
+          </div>
+          <q-btn style="-webkit-app-region: drag" flat @click="drawer = !drawer" round dense icon="menu" />
+          <q-btn style="-webkit-app-region: drag" flat @click="drawer = !drawer" round dense icon="minimize" />
+          <q-btn style="-webkit-app-region: drag" flat @click="drawer = !drawer" round dense icon="maximize" />
+          <q-btn style="-webkit-app-region: drag" flat @click="drawer = !drawer" round dense icon="close" />
+        </q-toolbar>
     </q-header>
-
     <q-drawer
-      v-model="leftDrawerOpen"
+      v-model="drawer"
       show-if-above
-      bordered
-      class="bg-grey-1"
+      :width="200"
+      :breakpoint="400"
     >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
+      <q-scroll-area style="height: calc(100% - 80px); margin-top: 80px; border-right: 1px solid #ddd">
+        <q-list padding>
+          <template v-for="item in essentialLinks" :key="item.order">
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon :name="item.icon" />
+              </q-item-section>
+              <q-item-section>
+                {{item.title}}
+              </q-item-section>
+            </q-item>
+          </template>
+        </q-list>
+      </q-scroll-area>
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+      <q-img class="absolute-top" 
+        src="https://www.bing.com/th?id=OHR.PrathameshJaju_ZH-CN2207606082_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp"
+        style="height: 80px">
+        <div class="absolute-bottom bg-transparent">
+          
+          <q-toolbar-title>
+            <q-avatar size="28px" class="q-mb-sm">
+              <img src="https://www.spark-app.store/res/static/img/slogo.png">
+            </q-avatar>
+            Spark Store
+          </q-toolbar-title>
+        </div>
+      </q-img>
     </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -53,47 +66,89 @@ import EssentialLink from 'components/EssentialLink.vue'
 
 const linksList = [
   {
-    title: 'Docs',
+    title: 'Home',
     caption: 'quasar.dev',
     icon: 'school',
     link: 'https://quasar.dev'
   },
   {
-    title: 'Github',
+    title: 'Network',
     caption: 'github.com/quasarframework',
     icon: 'code',
     link: 'https://github.com/quasarframework'
   },
   {
-    title: 'Discord Chat Channel',
+    title: 'Chat',
     caption: 'chat.quasar.dev',
     icon: 'chat',
     link: 'https://chat.quasar.dev'
   },
   {
-    title: 'Forum',
+    title: 'Music',
     caption: 'forum.quasar.dev',
     icon: 'record_voice_over',
     link: 'https://forum.quasar.dev'
   },
   {
-    title: 'Twitter',
+    title: 'Video',
     caption: '@quasarframework',
     icon: 'rss_feed',
     link: 'https://twitter.quasar.dev'
   },
   {
-    title: 'Facebook',
+    title: 'Graphics',
     caption: '@QuasarFramework',
     icon: 'public',
     link: 'https://facebook.quasar.dev'
   },
   {
-    title: 'Quasar Awesome',
+    title: 'Games',
     caption: 'Community Quasar projects',
     icon: 'favorite',
     link: 'https://awesome.quasar.dev'
-  }
+  },
+  {
+    title: 'Office',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'Reading',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'Development',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'Tools',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'Beautify',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'Others',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'Downloads',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  },
 ];
 
 import { defineComponent, ref } from 'vue'
@@ -102,7 +157,6 @@ export default defineComponent({
   name: 'MainLayout',
 
   components: {
-    EssentialLink
   },
 
   setup () {
--- ui/src/pages/Index.vue
+++ ui/src/pages/Index.vue
@@ -1,17 +1,116 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <div id="home">
+    <center>
+      <br><br>
+      <h3 >Spark Store</h3>
+      <h5 class="more-info">
+        Have more.
+      </h5>
+      <div class="buttons">
+       <!--
+          愿望墙: http://www.shenmo.tech:420/?p=91
+          应用问题反馈: http://www.shenmo.tech:420/?p=419
+          APP建议: http://www.shenmo.tech:420/?p=422
+          国际排名：http://distrowatch.com/table.php?distribution=deepin
+          
+          商店官网: https://www.spark-app.store/
+          所有的问题反馈换成这统一的入口
+          社区反馈中心: https://www.deepinos.org/t/spark-feedback
+          gitee反馈中心: https://gitee.com/deepin-community-store/feedback
+      -->
+        <!--
+        <button @click="Search" class="bt-feedback" disabled>搜索应用(暂未开放)</button>
+        <br>
+        <button @click="distrowatch" class="bt-feedback"><b>Deepin国际排名</b></button>
+        -->
+        <br>
+        <button @click="Wishwall" class="bt-feedback">愿望墙</button>
+        <br>
+        <button @click="FeedBack" class="bt-feedback">社区中心</button>
+        <br>
+        <button @click="Contri" class="bt-feedback">参与贡献</button>
+        <br>
+        <button @click="Donate" class="bt-feedback">随喜捐赠</button>
+        <br>
+        <button @click="Monitor" class="bt-feedback">节点监控</button>
+        <br>
+      </div>
+    </center>
+  </div>
+
 </template>
+
 
 <script>
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'PageIndex'
+    name: 'HelloWorld',
+    data() {
+      return {}
+    },
+    methods: {
+      FeedBack: function () {
+        window.open("https://www.deepinos.org/t/spark-feedback", "_self", "")
+      },
+      Wishwall: function () {
+        window.open("https://www.deepinos.org/t/Wish-wall", "_self", "")
+      },
+      Monitor: function () {
+        window.open("https://status.spark-app.store", "_self", "")
+      },
+      officialwebsite: function () {
+        window.open(" https://www.spark-app.store/", "_self", "")
+      },
+      distrowatch: function () {
+        window.open("https://distrowatch.com/table.php?distribution=deepin", "_self", "")
+      },
+      GiteeFeedBack: function () {
+        window.open("https://gitee.com/deepin-community-store/feedback", "_self", "")
+      },
+      Contri: function () {
+        window.open("https://gitee.com/deepin-community-store", "_self", "")
+      },
+      Donate: function () {
+        window.open("https://spark.deepinos.org.cn/", "_self", "")
+      },
+      Search: function () {
+        this.$router.push({name: "Search"})
+      }
+    }
 })
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #home {
+    border: transparent;
+    font-family: Bahnschrift;
+  }
+  .more-info {
+    color: gray;
+    font-weight: lighter;
+  }
+
+  .bt-feedback {
+    width: 300px;
+    padding: 10px;
+    margin: 15px;
+    /*margin-top: 40px;*/
+    border: transparent;
+    background: #F4F4F6;
+    transition: all 0.25s;
+    border-radius: 18px;
+    color: #6d6d6d;
+    -webkit-transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+    overflow: hidden;
+  }
+
+  .bt-feedback:hover {
+    box-shadow: 0 5px 15px rgba(0,0,0,0.3);
+    -webkit-transform: scale(1.07, 1.07);
+    transform: scale(1.07, 1.07);
+  }
+
+</style>
