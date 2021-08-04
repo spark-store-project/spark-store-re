8051d91bb606731c14ff42a49fe38f027beb36c9
robin <dequanzh@usc.edu>
2021-08-03 17:46:25 -0700

    spark store electron version init

---
+++ ui/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-view />
+</template>
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
+
+<style>
+.native-window-draggable {
+  -webkit-user-select: none;
+  -webkit-app-region: drag;
+}
+</style>
